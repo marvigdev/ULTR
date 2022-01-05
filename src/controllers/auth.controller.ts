7c9b02fddd658661d1e1105159654c2574a6cdbb
Marcos Vinicius <hello@marvig.dev>
2022-01-05 10:18:41 -0300

    Design changes

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -4,6 +4,7 @@ import { userService } from '../services/User.services';
 const authController = {
   login: async (req: Request, res: Response) => {
     try {
+      console.log(req.body);
       const user = await userService.getUserByUsername(req.body.username);
       await userService.passwordMatch(user, req.body.password);
       const jwtToken = await userService.generateJwt(user);
--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,7 @@ app.engine(
 app.set('view engine', 'hbs');
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(express.static(`${__dirname}/public`))
 
 app.use(AuthRouter);
 app.use(AppRouter);
